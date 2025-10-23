25a132e6c8c22b5680db633786a0ff0cdc7b94e7
Healtime2025 <smoriti@gmail.com>
2025-10-23 17:28:07 +0200

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // ⛔ Do NOT add: output: 'export'
+  // ⛔ Do NOT add rewrites that touch /api/*
+};
+export default nextConfig;
