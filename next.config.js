455d39a516aa59733e9ade44f8085ab54fd90234
Healtime2025 <smoriti@gmail.com>
2025-10-23 19:33:01 +0200

    Delete next.config.js

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // ⛔ Do NOT add: output: 'export'
-  // ⛔ Do NOT add rewrites that touch /api/*
-};
-export default nextConfig;
